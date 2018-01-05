1c95194955b6e02833fb701ac431d992be3abf5f
cfarrend <cfarrend@myune.edu.au>
2018-01-06 03:42:13 +1100

    Add JUnit and test case

---
+++ src/test/java/com/cfarrend/AusTravelSim/AusTravelSimTest.java
@@ -0,0 +1,17 @@
+package com.cfarrend.AusTravelSim;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Chris on 06/01/2018.
+ */
+public class AusTravelSimTest {
+
+    @Test
+    public void TrueIsTrue() {
+        assertEquals(true, true);
+    }
+
+}
