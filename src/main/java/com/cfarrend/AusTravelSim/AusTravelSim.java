d6454e0bd9c3fe91b39dff4764058790f7aa8161
cfarrend <cfarrend@myune.edu.au>
2018-01-06 00:46:59 +1100

    Add project code

---
+++ src/main/java/com/cfarrend/AusTravelSim/AusTravelSim.java
@@ -0,0 +1,10 @@
+package com.cfarrend.AusTravelSim;
+
+/**
+ * Created by Chris on 05/01/2018.
+ */
+public class AusTravelSim {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
