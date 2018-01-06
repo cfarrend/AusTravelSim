83bc4c87a9615892359905974510e8da19e14f29
Barkley <breynol5@myune.edu.au>
2018-01-06 11:44:05 +1100

    Mad little addition

--- src/main/java/com/cfarrend/AusTravelSim/AusTravelSim.java
+++ src/main/java/com/cfarrend/AusTravelSim/AusTravelSim.java
@@ -1,10 +1,57 @@
 package com.cfarrend.AusTravelSim;
 
+import java.util.Scanner;
+
 /**
  * Created by Chris on 05/01/2018.
  */
 public class AusTravelSim {
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hello world");
+
+        System.out.println("Areas to travel too: ");
+        System.out.println("\t1. Sydney\n\t2. Canberra\n\t3. Melbourne\n\t4. Brisbane\n\t5. Adelaide\n\t6. Perth\n\t7. Darwin\n\t8. Tamworth\n\t9. End Game");
+        System.out.print("Choose a City via #: ");
+
+        int selection = input.nextInt();
+        String currentLocation;
+
+        switch (selection) {
+
+                case 1:
+                    currentLocation = "You are now in: Sydeny";
+                    break;
+                case 2:
+                    currentLocation = "You are now in: Canberra";
+                    break;
+                case 3:
+                    currentLocation = "You are now in: Melbourne";
+                    break;
+                case 4:
+                    currentLocation = "You are now in: Brisbane";
+                    break;
+                case 5:
+                    currentLocation = "You are now in: Adelaide";
+                    break;
+                case 6:
+                    currentLocation = "You are now in: Perth";
+                    break;
+                case 7:
+                    currentLocation = "You are now in: Darwin";
+                    break;
+                case 8:
+                    currentLocation = "You are now in: Tamworth";
+                    break;
+                default:
+                    currentLocation = "You are now in: You tell me";
+                    break;
+
+        }
+        System.out.println("\n" + currentLocation);
+
     }
 }
